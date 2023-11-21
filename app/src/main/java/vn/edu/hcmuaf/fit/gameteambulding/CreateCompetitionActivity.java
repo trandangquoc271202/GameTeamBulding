a2df30e5e2e21cc14ffec370b177a82c6f6a7846
unknown <20130376@st.hcmuaf.edu.vn>
2023-11-21 22:13:34 +0700

    update create competition UI

---
+++ app/src/main/java/vn/edu/hcmuaf/fit/gameteambulding/CreateCompetitionActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.hcmuaf.fit.gameteambulding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateCompetitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_competition);
+    }
+}
