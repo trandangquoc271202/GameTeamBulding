dcf743e729c76cc65c59bf0948bc35c6d4d87f36
unknown <20130376@st.hcmuaf.edu.vn>
2023-11-25 13:46:31 +0700

    update function create contest

--- app/src/main/java/vn/edu/hcmuaf/fit/gameteambulding/CreateCompetitionActivity.java
+++ app/src/main/java/vn/edu/hcmuaf/fit/gameteambulding/CreateCompetitionActivity.java
@@ -3,12 +3,24 @@ package vn.edu.hcmuaf.fit.gameteambulding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateCompetitionActivity extends AppCompatActivity {
-
+    private View back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_competition);
+        back();
+    }
+
+    public void back() {
+        back = (View) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
